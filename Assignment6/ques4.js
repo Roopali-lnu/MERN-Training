94132e60b3d486bcfa4eec02c46a985c36d6b915
Roopali LNU <roopali.lnu@telusinternational.com>
2023-10-19 00:24:49 +0530

    Callback Function

---
+++ Assignment6/ques4.js
@@ -0,0 +1,20 @@
+//What are callback functions and why are they named so? Give one example.
+
+/*A callback function is a function that is passed as an argument to another function,
+ to be “called back” at a later time. A function that accepts other functions as arguments 
+ is called a higher-order function, which contains the logic for when the callback function gets executed*/
+
+ //eg 
+ function serverRequest(query, callback){
+    setTimeout(function(){
+      var response = query + "full!";
+      callback(response);
+    },5000);
+  }
+  
+  function getResults(results){
+    console.log("Response from the server: " + results);
+  }
+  
+  serverRequest("The glass is half ", getResults);
+  
---
+++ Assignment6/ques5.js
@@ -0,0 +1,16 @@
+setTimeout(() => console.log("I am a setTimeout func"), 0);
+function a(x) {
+    console.log("a called");
+    x();
+}
+const b = () => {
+    console.log("b called");
+}
+a(b);
+
+
+/*
+a called
+b called
+I am a setTimeout func
+*/
